53b96a015d921ad71850c695a308eb58e5722215
Jonas <56066318+OG-Jons@users.noreply.github.com>
2023-05-10 15:15:37 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <notifications />
-  <Suspense>
-    <template #default>
-      <div class="wrapper">
-        <SubscribeCard />
-      </div>
-    </template>
-    <template #fallback>
-      <div class="wrapper">
-        <SubscribeCardSkeleton /></div
-    ></template>
-  </Suspense>
+  <div>
+    <h1>Diese Seite ist veraltet</h1>
+    <h2>Der Mealbot wurde zu <a href="https://mealbot.chunk.moe">https://mealbot.chunk.moe</a> verschoben</h2>
+  </div>
 </template>
 
 <script setup>
-import SubscribeCard from './components/SubscribeCard.vue';
-import SubscribeCardSkeleton from './components/SubscribeCardSkeleton.vue';
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.location.href = 'https://mealbot.chunk.moe';
+})
 </script>
 
 <style scoped>
